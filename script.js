b19c59a5fa919c030e127305e84bc6d0b711a0a7
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-11-22 20:07:11 -0700

    css and fixing errors

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ if (now > scheduleElArray[i].data('hour')) {
     scheduleElArray[i].addClass('future')
 }
 }
+
+//save data from the save btn, hour and the container //
+
+let saveBttn = $('.save-icon');
+let containerEl = $('.container');
+let schedule9am = $('#9AM');
+let schedule10am = $('#10AM');
+let schedule11am = $('#11AM');
+let schedule12pm = $('#12PM');
+let schedule1pm = $('#1PM');
+let schedule2pm = $('#2PM');
+let schedule3pm = $('#3PM');
+let schedule4pm = $('#4PM');
+let schedule5pm = $('#5PM');
+
+let scheduleElArray = [schedule9am, schedule10am, schedule11am, schedule12pm, schedule1pm, schedule2pm, schedule3pm, schedule4pm, schedule5pm];
+
+renderLastRegistered();
+updateTime();
+setInterval(updateTime, 1000);
+
+function renderLastRegistered() {
+    for (let el of scheduleElArray) {
+        el.val(localStorage.getItem('time block' + el.data('hour')));
+    }
+}
