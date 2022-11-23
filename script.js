9eb1425af55878fce10ee647dc6cd850d817bdd8
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-11-22 21:46:00 -0700

    localstorate

--- script.js
+++ script.js
@@ -3,29 +3,3 @@ var currentdate = document.querySelector('#currentDay')
 
 currentdate.textContent = date
 
-
-// compareed each slot with the actual time //
-
-$(".time-div").each(function () {
-    var timeDiv = $(this).attr("id").split("-")[1];
-    
-    if (currentHour == timeDiv) {
-      $(this).addClass("present");
-      $(this).children(".description").addClass("white-text");
-    } else if (currentHour < timeDiv) {
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    } else if (currentHour > timeDiv) {
-      $(this).removeClass("future");
-      $(this).addClass("past");
-    }
-  });
-
-  //saving in the local store //
-  
-  $(".saveBtn").click(function (event) {
-    event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
-    localStorage.setItem(time, value);
-  });
