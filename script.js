a1d87f4ee306deb69be62a204c2d90aaa9acc128
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-11-23 19:18:32 -0700

    script

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ var currentdate = document.querySelector('#currentDay')
 
 currentdate.textContent = date
 
+var nine = $("#9am");
+var ten = $("#10am");
+var eleven = $("#11am");
+var twelve = $("#12pm");
+var one = $("#1pm");
+var two = $("#2pm");
+var three = $("#3pm");
+var four = $("#4pm");
+var five = $("#5pm");
