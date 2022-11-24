65844cf2160dcbaaa9a61cca1220af3d22c44fca
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-11-23 19:40:00 -0700

    script

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
+//updates time on the webpage
+function updateTIME() {
+    let today = moment();
+
+// time element in the header//
 var date = moment().format('dddd MMMM Do');
 var currentdate = document.querySelector('#currentDay')
 
 currentdate.textContent = date
 
-var nine = $("#9am");
-var ten = $("#10am");
-var eleven = $("#11am");
-var twelve = $("#12pm");
-var one = $("#1pm");
-var two = $("#2pm");
-var three = $("#3pm");
-var four = $("#4pm");
-var five = $("#5pm");
+//past, present and future element colors//
+
+let now = moment().format("kk");
+for (let i= 0; i < scheduleElArray.lenght; i++) {
+    scheduleElArray[i].removeClass("future past present");
+
+    if (now > scheduleElArray[i].data("hour")) {
+        scheduleElArray[i].addClass("past");
+    }
+        else if (now === scheduleElArray[i].attr("data-hour")) {
+            scheduleElArray[i].addClass("present");
+
+    } else {
+        scheduleElArray[i].addClass("future");
+    }
+    
+}
+
+}
+
+//textarea//
+
+let saveBttn = $(".save-icon");
+let containerEl = $(".container");
+let schedule9am = $("#9 AM");
+let schedule10am = $("#10 AM");
+let schedule11am = $("#11 AM");
+let schedule12pm = $("#12 PM");
+let schedule1pm = $("#1 PM");
+let schedule2pm = $("#2 PM");
+let schedule3pm = $("#3 PM");
+let schedule4pm = $("#4 PM");
+let schedule5pm = $("#5 PM");
+
