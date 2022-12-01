30235bb475a798a9f03d351947e592bee25ba865
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-12-01 11:54:34 -0700

    script

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ var currentdate = document.querySelector('#currentDay');
 
 currentdate.textContent = date
 
+var saveBtn = (".saveBtn");
+
+function timeBlockColor() {
+    var hour = moment().hour();
+
+}
+
+$(".saveBtn").on("click", function () {
+    // Get value of correct row description
+    var userText = $(this).siblings(".description").val();
+    var timeStamp = $(this).parent().attr("id");
+    // save in localStorage
+    localStorage.setItem(timeStamp, userText);
+});
+
+function schedule() 
+    
